1fd77bdcc78f8e1534109e665d0a52dee221d6bd
esaumatias <esaufreirematias@gmail.com>
2025-01-14 10:22:00 -0300

    feat - implementa tela inicial

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import logo from "../../Assets/Logo branca sem contorno.png";
+
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <section className="containerHome">
+      <header className="headerMenu">
+        <img src={logo} alt="LOGO ArcBio" />
+
+        <nav className="nav">
+          <ul className="ul">
+            <li className="li">
+              <a href="#Serviços" className="link">
+                Serviços
+              </a>
+            </li>
+            <li className="li">
+              <a href="#Portfólio" className="link">
+                Portfólio
+              </a>
+            </li>
+            <li className="li">
+              <a href="#Entre em contato" className="link">
+                Entre em contato
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="content">
+        <h1>
+          Arquitetura Sustentável e Exclusiva para Hotéis, Pousadas e
+          Residências Litorâneas
+        </h1>
+
+        <div className="subTitle">
+          <strong>Projetos de alto padrão</strong> que harmonizam estética rústica e
+          funcionalidade em harmonia com a natureza.
+        </div>
+
+        <button className="buttonHome">Solicitar Reunião</button>
+      </section>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Sobre/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>oila</section>
+  );
+};
+
+export default Home;
