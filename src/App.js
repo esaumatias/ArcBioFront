339af590c8b3aa165b6ec7e868eeb4723b6b53ef
esaumatias <esaufreirematias@gmail.com>
2025-01-14 20:38:54 -0300

    feat - implementa Projetos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import Home from './Pages/Home';
-import './App.css';
-import Sobre from './Pages/Sobre';
+import React from "react";
+import Home from "./Pages/Home";
+import "./App.css";
+import Sobre from "./Pages/Sobre";
+import Portifolio from "./Pages/Portifolio";
 
 const App = () => {
   return (
     <div className="App">
-     <Home />
-     <Sobre />
+      <Home />
+      <Sobre />
+      <Portifolio />
     </div>
   );
-}
+};
 
 export default App;
