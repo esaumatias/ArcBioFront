a392f6d339d4dc6a883ff17cd2a46971d4a30b67
esaumatias <esaufreirematias@gmail.com>
2025-01-16 12:15:14 -0300

    feat - responsividade footes

--- src/Pages/Footer/index.js
+++ src/Pages/Footer/index.js
@@ -1,25 +1,74 @@
 import React from "react";
-import logo from '../../Assets/Logo sem contorno com legenda2@4x.png';
+import logo from "../../Assets/Logo sem contorno com legenda2@4x.png";
+import iconWHats from "../../Assets/whatsapp-svgrepo-com (2).svg";
+import iconInsta from "../../Assets/insta-svgrepo-com.svg";
 
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="containerFooter">
-      <img src={logo} alt="logo da ArcBio" />
-      <div className="direitos">
-        © 2025 ArcBio Arquitetura & Consultoria Ambiental. Todos os direitos
-        reservados.
-      </div>
-      <div className="endereco">
-        <div className="principal">Escritório Tibau do Sul-RN</div>
-        <div className="secundario">Av. Três Poderes, 210 - Centro, Tibau do Sul - RN, 59178-000.</div>
-        <div className="containerSocial">
-          <div>@arcbio.rn</div>
-          <div>(84) 3246-4285</div>
+    <>
+      <footer className="containerFooter mobile">
+        <img src={logo} alt="logo da ArcBio" />
+        <div className="endereco">
+          <div className="principal">Escritório Tibau do Sul-RN</div>
+          <div className="secundario">
+            Av. Três Poderes, 210 - Centro, Tibau do Sul - RN, 59178-000.
+          </div>
         </div>
-      </div>
-    </footer>
+        <div className="line" />
+        <div className="endereco">
+          <div className="containerSocial">
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <div className="containerSOcial">
+                <img src={iconInsta} alt="" style={{ width: "20px" }} />
+              </div>
+              @arcbio.rn
+            </div>
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <div className="containerSOcial">
+                <img src={iconWHats} alt="" style={{ width: "18px" }} />
+              </div>
+              (84) 3246-4285
+            </div>
+          </div>
+        </div>
+        <div className="line" />
+        <div className="direitos">
+            © 2025 ArcBio Arquitetura & Consultoria Ambiental. Todos os direitos
+            reservados.
+          </div>
+      </footer>
+
+      <footer className="containerFooter web">
+        <img src={logo} alt="logo da ArcBio" />
+        <div className="direitos">
+          © 2025 ArcBio Arquitetura & Consultoria Ambiental. Todos os direitos
+          reservados.
+        </div>
+        <div className="line" />
+        <div className="endereco">
+          <div className="principal">Escritório Tibau do Sul-RN</div>
+          <div className="secundario">
+            Av. Três Poderes, 210 - Centro, Tibau do Sul - RN, 59178-000.
+          </div>
+          <div className="containerSocial">
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <div className="containerSOcial">
+                <img src={iconInsta} alt="" style={{ width: "20px" }} />
+              </div>
+              @arcbio.rn
+            </div>
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <div className="containerSOcial">
+                <img src={iconWHats} alt="" style={{ width: "18px" }} />
+              </div>
+              (84) 3246-4285
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
